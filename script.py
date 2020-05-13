fd43ff9e49cf7c71ce7c3a7c12b517bc45a45693
divyanshu <60056983+divyanshu-in@users.noreply.github.com>
2020-05-13 16:19:34 +0530

    Update script.py

--- script.py
+++ script.py
@@ -56,33 +56,40 @@ class operations:
         indexes = list(map(int, ((input("enter file indexes to rename>> ").split(" ")))))
         torename = list(map(lambda x: self.files[x - 1], indexes))
         for i in torename:
-            newname = str(input("rename " + i + "as>> " ))
+            print(str(torename.index(i) + 1) + "/" + str(len(indexes)))
+            newname = str(input("rename " + i + " as>> " ))
             os.rename (i, newname)
+            print(colors.fg.green, i + " has been renamed as >> " + newname)
+            
     
     def delete(self):
         indexes = list(map(int, ((input("enter file indexes to delete>> ").split(" ")))))
         todelete = list(map(lambda x: self.files[x - 1], indexes))
         for i in todelete:
+            print(str(todelete.index(i) + 1) + "/" + str(len(indexes)))
             try:
                 os.remove(i)
             except IsADirectoryError:
                 os.rmdir(i)
-
+            print(colors.fg.green, "deleted ", i)
     def copy(self):
         indexes = list(map(int, ((input("enter file indexes to copy>> ").split(" ")))))
         tocopy = list(map(lambda x: self.files[x - 1], indexes))
 
         for i in tocopy:
-
+            print(str(tocopy.index(i) + 1) + "/" + str(len(indexes)))
+            print("copying", i)
             path = str(input("where to copy (path/same) >> "))
             command = 'cp -arv ' + copypath + " " + path
             os.system(command)
+            print(colors.fg.green, i, "has been copyied to", path)
 
 
     def move(self):
         indexes = list(map(int, ((input("enter file indexes to move>> ").split(" ")))))
         tomove = list(map(lambda x: self.files[x - 1], indexes))
         for i in tomove:
+            print(colors.fg.green, str(tomove.index(i) + 1) + "/" + str(len(indexes)))
             command = "mv " + i + " " + input("where to move (path/same) >> ")
             os.system(command)
 
@@ -92,10 +99,15 @@ class operations:
         index = int(input("how many folders to create>> "))
         for i in range(index):
             if ask == 1:
-                os.mkdir(input("enter name of folder >> "))
+                foldername = input("enter name of folder >> ")
+                os.mkdir(foldername)
+                print(colors.fg.green, foldername, "has been created")
             elif ask ==2:
-                command = 'cat > ' + input("Enter name of file along with extension>> ")
+                filename = input("Enter name of file along with extension>> ")
+                command = 'cat > ' + filename
                 os.system(command)
+                print(colors.fg.green, filename, "has been created")
+
                 
     def changefolder(self):
         changeCWD()
