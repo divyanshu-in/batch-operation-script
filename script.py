24058a13ea864e6b73c56bbd1140b9264646527d
aakash37 <aakashsingh1407@gmail.com>
2020-05-17 17:35:20 +0530

    update main()

--- script.py
+++ script.py
@@ -8,13 +8,13 @@ color = {
     "red": "\033[31m",  # for errors
     "green": "\033[32m",
     "yellow": "\033[33m",  # for input
-    "blue": "\033[34m",
+    "blue": "\033[34m",  # for indication
     "magenta": "\033[35m",
-    "cyan": "\033[36m",  # for indication
+    "cyan": "\033[36m",
     "reset": "\033[0m",
 }
 
-
+# TODO: catch permission errors
 # TODO: add usage of a global variable to hold the path
 global_path = ""
 
@@ -32,7 +32,7 @@ def show_dirs(path=os.getcwd()):
     Returns: 
     None 
     """
-    print(color["cyan"] + "Contents of the current directory")
+    print(color["blue"] + "Contents of the current directory")
     filelist = os.listdir(path)
     filelist.sort(key=lambda x: x.lower())
     # index padding
@@ -48,7 +48,13 @@ def show_dirs(path=os.getcwd()):
 
     scr_width = int(os.get_terminal_size()[0])
     try:
-        mlen = max(len(word) for word in filelist) + 1
+        mlen = (
+            max(
+                len(word) + 1 if os.path.isdir(word) else len(word)
+                for word in filelist
+            )
+            + 1
+        )
     except ValueError:
         mlen = 1
     cols = scr_width // mlen
@@ -60,17 +66,17 @@ def show_dirs(path=os.getcwd()):
     lst = []
     for count, _file in enumerate(filelist, start=1):
         last = True  # last line
-        # directories(blue)
+        # directories(cyan)
         if os.path.isdir(path + os.sep + _file):
             _file = _file + os.sep
             st = "[{0:>{ind}}] {1:<{mlen}}".format(
                 str(count), _file, mlen=mlen, ind=ind
             )
             if scr_width - (abs(len(line) - cols * 5) % scr_width) > len(st):
-                line = line + color["blue"] + st
+                line = line + color["cyan"] + st
             else:
                 lst.append(line)
-                line = color["blue"] + st
+                line = color["cyan"] + st
                 last = False
         # executeable files(yellow)
         elif os.access(path + os.sep + _file, os.X_OK):
@@ -115,7 +121,7 @@ def navmodedir():
     Returns: 
     None 
     """
-    print(color["cyan"] + "Current working directory is> " + os.getcwd())
+    print(color["blue"] + "Current working directory is> " + os.getcwd())
     while True:
         show_dirs(os.getcwd())
         print(
@@ -123,7 +129,6 @@ def navmodedir():
             "Enter directory index to move inside",
             "Press 'u' to exit current directory",
             "'x' to exit nav mode",
-            "",
             sep="\n",
         )
         dir_action = input("Select >> ").lower()
@@ -131,17 +136,16 @@ def navmodedir():
             _files = os.listdir(os.getcwd())
             _files.sort(key=lambda x: x.lower())
             filename = _files[int(dir_action) - 1]
-            print(filename)
             os.chdir(str(os.getcwd()) + os.sep + filename)
-            print(color["cyan"], "directory ->>> ", os.getcwd())
+            print(color["blue"] + "Directory --> ", os.getcwd())
         elif dir_action == "u":
             foldername = os.path.split(os.getcwd())[0]
             os.chdir(foldername)
-            print(color["cyan"], "directory ->>> ", os.getcwd())
+            print(color["blue"] + "Directory --> ", os.getcwd())
         elif dir_action == "x":
             break
         else:
-            print(color["red"] + "INVALID INPUT   !!! " + color["reset"])
+            print(color["red"] + "INVALID INPUT !!!" + color["reset"])
             continue
 
 
@@ -179,7 +183,6 @@ def directory_ask(change_dir=False):
     else:
         print(color["yellow"] + "Enter full path of the directory...")
         os.chdir(str(input("path>> ")))
-    show_dirs(os.getcwd())
 
 
 def select_op():
@@ -203,7 +206,7 @@ def select_op():
         " 3. Copy",
         " 4. Move",
         " 5. Create new folder/s, file/s",
-        " 6. Enter into a directory",
+        " 6. Enter a directory",
         " 7. Enter navigation mode",
         " 9. Change directory",
         "99. EXIT",
@@ -252,9 +255,9 @@ class Operations:
             print(color["yellow"] + "Enter file indexes to rename")
             indexes = self.get_index()
             if indexes is None:
-                print(color["red"] + "ERROR No file selected"+color["reset"])
+                print(color["red"] + "ERROR No file selected" + color["reset"])
                 continue
-            elif indexes == -1:
+            if indexes == -1:
                 break
             print(color["yellow"])
             dont_over = (
@@ -268,13 +271,26 @@ class Operations:
                     if os.path.exists(newname):
                         if (
                             input(_file + " already exists overwrite y/[n]?")
-                            .strip().lower() == "y"
+                            .strip()
+                            .lower()
+                            == "y"
                         ):
-                            os.rename(_file, newname)
+                            try:
+                                os.rename(_file, newname)
+                            except IsADirectoryError as e:
+                                print(color["red"] + e + color["yellow"])
+                            except NotADirectoryError as e:
+                                print(color["red"] + e + color["yellow"])
+
                         else:
                             continue
                 else:
-                    os.rename(_file, newname)
+                    try:
+                        os.rename(_file, newname)
+                    except IsADirectoryError as e:
+                        print(color["red"] + e + color["yellow"])
+                    except NotADirectoryError as e:
+                        print(color["red"] + e + color["yellow"])
             break
 
     def delete(self):
@@ -282,25 +298,34 @@ class Operations:
             print("Enter file indexes to delete")
             indexes = self.get_index()
             if indexes is None:
-                print(color["red"] + "ERROR No file selected"+color["reset"])
+                print(color["red"] + "ERROR No file selected" + color["reset"])
                 continue
-            elif indexes == -1:
+            if indexes == -1:
                 break
             todelete = list(map(lambda x: self.files[x - 1], indexes))
             print(color["yellow"])
             del_confirm = (
-                input("Ask confirmaton to delete y/[n]?").strip().lower()
-                == "y"
+                input("Ask confirmaton to delete y/[n]?").strip().lower() == "y"
             )
             for i in todelete:
                 if os.path.isdir(i):
                     if del_confirm:
-                        if (input("Delete directory "+ i +" [y]/n?").strip().lower() == "n"):
+                        if (
+                            input("Delete directory " + i + " [y]/n?")
+                            .strip()
+                            .lower()
+                            == "n"
+                        ):
                             continue
                     shutil.rmtree(i, ignore_errors=True)
                 else:
                     if del_confirm:
-                        if (input("Delete file "+ i +" [y]/n?").strip().lower() == "n"):
+                        if (
+                            input("Delete file " + i + " [y]/n?")
+                            .strip()
+                            .lower()
+                            == "n"
+                        ):
                             continue
                     os.remove(i)
             break
@@ -332,7 +357,6 @@ class Operations:
         dir_index = int(input("Enter directory index >>"))
         filename = files[int(dir_index) - 1]
         os.chdir(str(os.getcwd()) + os.sep + filename)
-        show_dirs(os.getcwd())
 
     @staticmethod
     def create():
@@ -373,33 +397,52 @@ def main(path=os.getcwd()):
                |_|    
         """,
     )
+    directory_ask(False)
     filelist = os.listdir(path)
     filelist.sort(key=lambda x: x.lower())
     oper_files = Operations(filelist)
+    show_dirs(os.getcwd())
     while True:
-        oprselection = select_op()
         try:
-            if oprselection == 1:
-                oper_files.rename()
-            elif oprselection == 2:
-                oper_files.delete()
-            elif oprselection == 3:
-                oper_files.copy()
-            elif oprselection == 4:
-                oper_files.move()
-            elif oprselection == 5:
-                oper_files.create()
-            elif oprselection == 6:
-                oper_files.enter_dir()
-            elif oprselection == 7:
-                navmodedir()
-            elif oprselection == 9:
-                directory_ask(True)
-            elif oprselection == 99:
-                break
-            else:
-                continue
-        except IndexError:
+            oprselection = select_op()
+        except ValueError:
+            print(color["red"] + "INVALID INPUT !!!" + color["reset"])
+            continue
+        if oprselection == 1:
+            oper_files.rename()
+            show_dirs(os.getcwd())
+        elif oprselection == 2:
+            oper_files.delete()
+            show_dirs(os.getcwd())
+        elif oprselection == 3:
+            oper_files.copy()
+            show_dirs(os.getcwd())
+        elif oprselection == 4:
+            oper_files.move()
+            show_dirs(os.getcwd())
+        elif oprselection == 5:
+            oper_files.create()
+            show_dirs(os.getcwd())
+        elif oprselection == 6:
+            oper_files.enter_dir()
+            filelist = os.listdir(path)
+            filelist.sort(key=lambda x: x.lower())
+            oper_files = Operations(filelist)
+            show_dirs(os.getcwd())
+        elif oprselection == 7:
+            navmodedir()
+            filelist = os.listdir(path)
+            filelist.sort(key=lambda x: x.lower())
+            oper_files = Operations(filelist)
+        elif oprselection == 9:
+            directory_ask(True)
+            show_dirs(os.getcwd())
+            filelist = os.listdir(path)
+            filelist.sort(key=lambda x: x.lower())
+            oper_files = Operations(filelist)
+        elif oprselection == 99:
+            break
+        else:
             continue
 
 
@@ -409,7 +452,6 @@ if __name__ == "__main__":
             os.chdir(sys.argv[1])
     except IndexError:
         pass
-    directory_ask(False)
     main()
 
 else:
